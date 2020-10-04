9d315391efe84adb95331b24100bf977c0b737f6
zARODz11z <andrewrod003@gmail.com>
2020-10-04 10:54:05 -0700

    each movie can be clicked on and a sub view with the youtube trailer will load.

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixter.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -10,15 +11,22 @@ import android.view.View;
 //import android.annotation.NonNull;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixter.DetailActivity;
 import com.example.flixter.R;
 import com.example.flixter.models.Movie;
 
+import org.parceler.Parcels;
+
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     Context context;
@@ -55,6 +63,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -65,9 +74,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -81,7 +91,19 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             }
 
 //            Glide.with(context).load(imageUrl).into(ivPoster);
-            Glide.with(context).load(imageUrl).placeholder(R.drawable.placeholder).into(ivPoster);
+            int radius = 30;
+            int margin = 5;
+            Glide.with(context).load(imageUrl).transform(new RoundedCornersTransformation(radius,margin)).placeholder(R.drawable.placeholder).into(ivPoster);
+
+            //1. register click listener on whole row
+            container.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    //2. go to new activity when tapped
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,21 +3,30 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+    int movieId;
     String backdroppath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    //needed for parcelable
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdroppath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
@@ -42,5 +51,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
